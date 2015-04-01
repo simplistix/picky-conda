9e9c035cba4adb2100e909f77a9ec60402877cab
Chris Withers <chris@simplistix.co.uk>
2015-04-01 14:54:29 -0700

    Initial requirements for picky itself.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ base_dir = os.path.dirname(__file__)
 
 setup(
     name='picky',
-    version='0.0dev',
+    version='0.0.dev0',
     author='Chris Withers',
     author_email='chris@simplistix.co.uk',
     license='MIT',
