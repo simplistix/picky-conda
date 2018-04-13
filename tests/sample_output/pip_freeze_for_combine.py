f280322bab2ad95ac491cb601b8b5c38b3f771ec
Chris Withers <chris@simplistix.co.uk>
2018-04-13 08:18:34 +0100

    kill off the old picky stuff

--- tests/sample_output/pip_freeze_for_combine.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-sys.stdout.write('''\
-d==5
-# some other comment
-b==4.1
-''')
--- tests/sample_output/pip_freeze_git.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-sys.stdout.write(
-    '-e git+https://github.com/Simplistix/picky.git'
-    '@181903dc9a100ead5879ebc0ed81d12145be68d9#egg=picky-master\n'
-)
-
-sys.exit(0)
