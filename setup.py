2bdae13f2e9d8dc09b596606d018d2ef03733e33
Chris Withers <chris@simplistix.co.uk>
2018-04-19 00:15:58 +0100

    Fresh attempt at packaging and CI

--- setup.py
+++ setup.py
@@ -1,45 +1,24 @@
-import os, sys
+import os
 
 from setuptools import setup, find_packages
 
 base_dir = os.path.dirname(__file__)
 
+description = "A tool for locking down your conda package usage."
+
 setup(
     name='picky-conda',
     version='2.0.0dev',
     author='Chris Withers',
-    author_email='chris@simplistix.co.uk',
+    author_email='chris@withers.org',
     license='MIT',
-    description=(
-        "A tool for checking versions of packages used by conda or pip "
-        "are as specified in their requirements files."
-    ),
-    long_description=open(os.path.join(base_dir,'docs','description.rst')).read(),
+    description=description,
+    long_description=description,
     url='https://github.com/Simplistix/picky-conda',
-    classifiers=[
-        # 'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
-    extras_require={
-        'test': [
-            'testfixtures',
-            'nose',
-            'nose_fixes',
-            'coveralls',
-            ],
-        'build': ['sphinx', 'pkginfo', 'setuptools-git', 'twine', 'wheel']
-},
+    install_requires=['PyYAML'],
     entry_points = {
         'console_scripts': [
             'picky = picky.main:main',
