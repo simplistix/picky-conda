c5dcd14df9f0d31caa292f1a0def6a46b1547bf6
Chris Withers <chris@simplistix.co.uk>
2018-04-20 08:55:12 +0100

    travis whackamole

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ description = "A tool for locking down your conda package usage."
 
 setup(
     name='picky-conda',
-    version='2.0.0dev4',
+    version='2.0.0dev5',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
